b3a1cc504a574e349da54cf510083594d9a401ae
Javokhir Kubaev <jkubaev7@gmail.com>
2025-08-24 19:57:00 +0200

    Parsing is in progress

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javokhir <javokhir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkubaev <jkubaev@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 20:14:03 by jkubaev           #+#    #+#             */
-/*   Updated: 2025/08/24 11:59:00 by javokhir         ###   ########.fr       */
+/*   Updated: 2025/08/24 19:56:24 by jkubaev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,26 +41,63 @@ t_token	*ft_tokenize(char *s)
 
 //	tokens to build an AST,
 
-void	build_ast(t_token *list, t_node *node)
+t_node	*create_node(t_node_type type, t_token **current_token)
 {
+	t_node	*new_node;
+	t_token	*tmp;
+	int i;
+
+	tmp = *current_token;
+	new_node = malloc(sizeof(t_node));
+	if (!new_node)
+		exit(1);
+	new_node->type = type;
+	if (type == COMMAND)
+	{
+		i = 0;
+		new_node->cmd.name = ft_strdup(tmp->value);
+		tmp = tmp->next;
+		new_node->cmd.args = malloc(sizeof(char **) * (i + 1));
+		if (!new_node->cmd.args)
+			exit(1);
+		while (tmp->type != T_PIPE && tmp->type != T_EOF)
+		{
+			new_node->cmd.args[i++] = ft_strdup(tmp->value);
+			// should be thinked to clean all the alocated memories, we need some functions for cleaning
+			new_node->cmd.args = realloc(new_node->cmd.args, i + 1);
+			// should be checked allocation fails and if fails, free previous array elements
+			tmp = tmp->next;
+		}
+		new_node->cmd.args[i] = NULL;
+	}
+	*current_token = tmp;
+	return (new_node);
+}
+
+t_node	*build_ast(t_token *list)
+{
+	t_node	*prev_node;
+	t_node	*current_node;
+
 	while (list->type != T_EOF)
 	{
 		if (list->type == T_WORD)
+			prev_node = create_node(COMMAND, &list);
+		if(list->type == T_PIPE)
 		{
-			node->type = list->type;
-			node->cmd.name = T_WORD;
+			current_node = create_node(PIPE, &list);
+			current_node->pipe.left = prev_node;
+			prev_node->pipe.right = build_ast(list->next);
 		}
-		else if(list->type == T_PIPE)
-		{
-			build_ast(list->next, node->next);
-		}
-		list = list->next;
 	}
+	return (current_node);
 }
 
 void	ft_parse(char *input)
 {
 	t_token	*token_list;
+	t_node	*nodes;
 
 	token_list = ft_tokenize(input);
+	nodes = build_ast(token_list);
 }
