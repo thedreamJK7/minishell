7ae3ac22994bf4f6089a002e60d114aa58bde25b
yingzhan25 <3146589226@qq.com>
2025-08-27 17:28:07 +0200

    fix error in redirection

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: yingzhan <yingzhan@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 20:14:03 by jkubaev           #+#    #+#             */
-/*   Updated: 2025/08/26 17:29:36 by yingzhan         ###   ########.fr       */
+/*   Updated: 2025/08/27 17:03:33 by yingzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@ t_token	*ft_tokenize(char *s)
 	return (list);
 }
 
-//	tokens to build an AST,
-
-t_node	*create_node(t_node_type type, t_token **current_token)
+t_node	*create_cmd_node(t_token **list, int empty)
 {
 	t_node	*new_node;
 	t_token	*tmp;
@@ -50,83 +48,160 @@ t_node	*create_node(t_node_type type, t_token **current_token)
 	new_node = malloc(sizeof(t_node));
 	if (!new_node)
 		exit(1);//direct exit or return and exit in build_ast()?
-	new_node->type = type;
-	if (type == COMMAND)
+	new_node->type = COMMAND;
+	if (empty)
 	{
-		i = 0;
-		tmp = *current_token;
-		while (tmp && tmp->type == T_WORD)
-		{
-			i++;
-			tmp = tmp->next;
-		}
-		new_node->cmd.args = malloc(sizeof(char *) * (i + 1));
-		if (!new_node->cmd.args)
-			exit(1);
-		i = 0;
-		while (*current_token && (*current_token)->type == T_WORD)
-		{
-			new_node->cmd.args[i++] = ft_strdup((*current_token)->value);
-			// should be thinked to clean all the alocated memories, we need some functions for cleaning
-			// should be checked allocation fails and if fails, free previous array elements
-			*current_token = (*current_token)->next;
-		}
-		new_node->cmd.args[i] = NULL;
+		new_node->cmd.args = malloc(sizeof(char *));
+		if (!new_node)
+			exit(1);//cleanup memery
+		new_node->cmd.args[0] = NULL;
+		return (new_node);
 	}
-	else if (type == REDIR)
+	i = 0;
+	tmp = *list;
+	while (tmp && tmp->type == T_WORD)
 	{
-		if ((*current_token)->type == T_LESS)
-			new_node->redir.redir_type = IN;
-		else if ((*current_token)->type == T_GREAT)
-			new_node->redir.redir_type = OUT;
-		else if ((*current_token)->type == T_DGREAT)
-			new_node->redir.redir_type = APPEND;
-		else if ((*current_token)->type == T_DLESS)
-			new_node->redir.redir_type = HEREDOC;
-		*current_token = (*current_token)->next;
-		if (*current_token && (*current_token)->type == T_WORD)
-		{
-			new_node->redir.file = ft_strdup((*current_token)->value);
-			*current_token = (*current_token)->next;
-		}
-		else
-		{
-			printf("Syntax error: Invalid filename/limiter of redirection");
-			free(new_node);
-			exit(1);
-		}
-		new_node->redir.child = NULL;
+		i++;
+		tmp = tmp->next;
 	}
+	new_node->cmd.args = malloc(sizeof(char *) * (i + 1));
+	if (!new_node->cmd.args)
+		exit(1);//cleanup memery
+	i = 0;
+	while (*list && (*list)->type == T_WORD)
+	{
+		new_node->cmd.args[i++] = ft_strdup((*list)->value);
+		// should be thinked to clean all the alocated memories, we need some functions for cleaning
+		// should be checked allocation fails and if fails, free previous array elements
+		*list = (*list)->next;
+	}
+	new_node->cmd.args[i] = NULL;
 	return (new_node);
+}
+
+t_node	*create_redir_node(t_token **list)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if (!new_node)
+		exit(1);//direct exit or return and exit in build_ast()?
+	new_node->type = REDIR;
+	if ((*list)->type == T_LESS)
+		new_node->redir.redir_type = IN;
+	else if ((*list)->type == T_GREAT)
+		new_node->redir.redir_type = OUT;
+	else if ((*list)->type == T_DGREAT)
+		new_node->redir.redir_type = APPEND;
+	else if ((*list)->type == T_DLESS)
+		new_node->redir.redir_type = HEREDOC;
+	*list = (*list)->next;
+	if (*list && (*list)->type == T_WORD)
+	{
+		new_node->redir.file = ft_strdup((*list)->value);
+		*list = (*list)->next;
+	}
+	else
+	{
+		printf("Syntax error: Invalid filename/limiter of redirection");
+		free(new_node);
+		exit(1);//cleanup memery
+	}
+	new_node->redir.child = NULL;
+	return (new_node);
+}
+
+void	add_cmd_args(t_node *cmd_node, t_token **list)
+{
+	int		i;
+	int		j;
+	char	**new_array;
+	char	*value;
+
+	i = 0;
+	while (cmd_node->cmd.args && cmd_node->cmd.args[i])
+		i++;
+	new_array = malloc(sizeof(char *) * (i + 2));
+	if (!new_array)
+		exit(1);//cleanup memery
+	j = 0;
+	while (j < i)
+	{
+		new_array[j] = cmd_node->cmd.args[j];
+		j++;
+	}
+	value = ft_strdup((*list)->value);
+	if (!value)
+		exit(1);//cleanup memory
+	new_array[i] = value;
+	new_array[i + 1] = NULL;
+	free(cmd_node->cmd.args);
+	cmd_node->cmd.args = new_array;
+	*list = (*list)->next;
+}
+//cmd_node is always created at the beginning, even if there is no cmd
+t_node	*find_left(t_token **list)
+{
+	t_node	*cmd_node;
+	t_node	*redir_node;
+	t_node	*last_redir;
+
+	cmd_node = NULL;
+	redir_node = NULL;
+	last_redir = NULL;
+	while (*list && (*list)->type != T_EOF && (*list)->type != T_PIPE)
+	{
+		if ((*list)->type == T_WORD)
+		{
+			if (!cmd_node)
+				cmd_node = create_cmd_node(list, 0);
+			else
+				add_cmd_args(cmd_node, list);
+		}
+		else if ((*list)->type == T_LESS || (*list)->type == T_GREAT || (*list)->type == T_DLESS || (*list)->type == T_DGREAT)
+		{
+			redir_node = create_redir_node(list);
+			if (!cmd_node)
+				cmd_node = create_cmd_node(list, 1);
+			if (last_redir)
+				redir_node->redir.child = last_redir;
+			else
+				redir_node->redir.child = cmd_node;
+			last_redir = redir_node;
+		}
+	}
+	if (last_redir)
+		return (last_redir);
+	else
+		return (cmd_node);
 }
 
 t_node	*build_ast(t_token **list)
 {
 	t_node	*left_node;
 	t_node	*pipe_node;
-	t_node	*redir_node;
 
+	if (!(*list) || (*list)->type == T_EOF)
+		return (NULL);
 	left_node = NULL;
-	while (*list && (*list)->type != T_EOF)
+	left_node = find_left(list);
+	if (*list && (*list)->type == T_PIPE)
 	{
-		if ((*list)->type == T_WORD)
-			left_node = create_node(COMMAND, list);
-		else if ((*list)->type == T_LESS || (*list)->type == T_GREAT || (*list)->type == T_DLESS || (*list)->type == T_DGREAT)
+		if ((*list)->next->type == T_PIPE)
 		{
-			redir_node = create_node(REDIR, list);
-			if (!left_node && (*list)->type == T_WORD)
-				left_node = create_node(COMMAND, list);
-			redir_node->redir.child = left_node;
-			left_node = redir_node;
+			printf("Syntax error: Invalid pipeline");
+			free_ast(left_node);
+			clean_tokens(list);
+			exit(1);//cleanup memery
 		}
-		else if((*list)->type == T_PIPE)
-		{
-			pipe_node = create_node(PIPE, list);
-			pipe_node->pipe.left = left_node;
-			*list = (*list)->next;
-			pipe_node->pipe.right = build_ast(list);
-			left_node = pipe_node;
-		}
+		pipe_node = malloc(sizeof(t_node));
+		if (!pipe_node)
+			exit(1);//direct exit or return and exit in build_ast()?
+		pipe_node->type = PIPE;
+		pipe_node->pipe.left = left_node;
+		*list = (*list)->next;
+		pipe_node->pipe.right = build_ast(list);
+		left_node = pipe_node;
 	}
 	return (left_node);
 }
