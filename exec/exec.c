4b513ba8d3656a2454b9688499f30f1d488052bf
Javokhir Kubaev <jkubaev7@gmail.com>
2025-09-24 18:47:34 +0200

    builtin echo is fixed

--- exec/exec.c
+++ exec/exec.c
@@ -3,15 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yingzhan <yingzhan@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: jkubaev <jkubaev@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 20:13:50 by jkubaev           #+#    #+#             */
-/*   Updated: 2025/09/23 17:13:27 by yingzhan         ###   ########.fr       */
+/*   Updated: 2025/09/24 17:55:34 by jkubaev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../includes/minishell.h"
+
+int setup_redirections(t_redir_token *redirs)
+{
+	t_redir_token	*tmp;
+	int				fd;
+
+	tmp = redirs;
+	while (tmp)
+	{
+		if (tmp->redir_type == IN)
+		{
+			fd = open(tmp->file, O_RDONLY);
+			if (fd == -1)
+				return (perror(tmp->file), GENERAL_ERROR);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		if (tmp->redir_type == OUT)
+		{
+			fd = open(tmp->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+				return (perror(tmp->file), GENERAL_ERROR);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		if (tmp->redir_type == APPEND)
+		{
+			fd = open(tmp->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd == -1)
+				return (perror(tmp->file), GENERAL_ERROR);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		if (tmp->redir_type == HEREDOC)
+		{
+			fd = tmp->heredoc_fd;
+			if (fd == -1)
+				return (perror(tmp->file), GENERAL_ERROR);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
 
 int	is_builtin(char *arg)
 {
@@ -30,11 +75,33 @@ int	is_builtin(char *arg)
 
 int	exec_simple_command(t_node *cmd, t_shell *shell)
 {
-	if (cmd->cmd.redir_token)
-		return (exec_non_builtin(cmd, shell));
+	// if (cmd->cmd.redir_token)
+	// 	return (exec_non_builtin(cmd, shell));
 	if (cmd->cmd.cmd && !is_builtin(cmd->cmd.cmd[0]))
-		return (exec_builtin(shell, cmd->cmd.cmd));
-	return (exec_non_builtin(cmd, shell));
+	{
+		int	status;
+		int pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			return (GENERAL_ERROR);
+		}
+		if (!pid)
+		{
+			if (setup_redirections(cmd->cmd.redir_token))
+				exit (1);
+			exit(exec_builtin(shell, cmd->cmd.cmd));
+		}
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			shell->exit_code = WEXITSTATUS(status);
+			return (WEXITSTATUS(status));
+		}
+	}
+	else
+		return (exec_non_builtin(cmd, shell));
+	return (shell->exit_code);
 }
 
 int	execute(t_node *node, t_shell *shell)
